f24188143407def9bf09dff9e4bc17b2e1e7357d
gabee1987 <gabee1987@gmail.com>
2017-06-08 21:08:50 +0200

    Fixed minor bugs

--- static/js/extras.js
+++ static/js/extras.js
@@ -1,10 +1,36 @@
 $(document).ready(function() {
-    $('ul').sortable({
+    $('ol').sortable({
         revert: true,
         cancel: "#sortable li"
     });
 
     $('.trash i').click(function() {
-        $('.notepad ul').empty();
+        $('.notepad ol').empty();
     })
+
+    //var searchText = document.getElementById("search_text");
+    //var textToHighlight = document.documentElement.innerHTML.indexOf(searchText);
+    //function handleKeyPress(e){
+        //var key=e.keyCode || e.which;
+        //if (key==13){
+            //textToHighlight.css("background-color", "yellow")
+        //}
+    //}
+
+    function startTime() {
+        var today = new Date();
+        var h = today.getHours();
+        var m = today.getMinutes();
+        var s = today.getSeconds();
+        m = checkTime(m);
+        s = checkTime(s);
+        document.getElementById('clock').innerHTML =
+        h + ':' + m + ':' + s;
+        var t = setTimeout(startTime, 500);
+    }
+    function checkTime(i) {
+        if (i < 10) {i = '0' + i};  // add zero in front of numbers < 10
+        return i;
+    }
+
 });
